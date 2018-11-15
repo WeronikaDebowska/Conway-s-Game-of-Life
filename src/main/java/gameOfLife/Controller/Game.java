a5cd6b80eb76d0a7ef77258b432c0174b637b115
WeronikaDebowska <weronika.debowska@gmail.com>
2018-11-15 13:06:50 +0100

    comments deleted

--- src/main/java/gameOfLife/Controller/Game.java
+++ src/main/java/gameOfLife/Controller/Game.java
@@ -16,77 +16,7 @@ public class Game {
         currentGeneration = generation;
         numberOfRows = currentGeneration.getNumberOfRows();
         numberOfColumns = currentGeneration.getNumberOfColumns();
-//        findAllCellsNeighbours();
     }
-
-//    private void findAllCellsNeighbours() {
-//
-//        for (int row = 1; row < numberOfRows - 1; row++) {
-//            for (int column = 1; column < numberOfColumns - 1; column++) {
-//                findCellsNeighbours(row, column);
-//            }
-//        }
-//    }
-//
-//    private void findCellsNeighbours(int row, int column) {
-//
-//
-//        if (row == 0 && column == 0) {                           //upper left corner
-//            getCell(row, column).setNeighbours(new Cell[]{
-//                    getCell(numberOfRows - 1, numberOfColumns - 1), getCell(numberOfRows - 1, column), getCell(numberOfRows - 1, column + 1),
-//                    getCell(row, numberOfColumns - 1), getCell(row, column + 1),
-//                    getCell(row + 1, numberOfColumns - 1), getCell(row + 1, column), getCell(row + 1, column + 1)
-//            });
-//        } else if (row == 0 && column == numberOfColumns - 1) {   // upper right corner
-//            getCell(row, column).setNeighbours(new Cell[]{
-//                    getCell(numberOfRows - 1, column - 1), getCell(numberOfRows - 1, column), getCell(numberOfRows - 1, 0),
-//                    getCell(row, column - 1), getCell(row, 0),
-//                    getCell(row + 1, column - 1), getCell(row + 1, column), getCell(row + 1, 0)
-//            });
-//        } else if (row == numberOfRows - 1 && column == 0) {         //down left corner
-//            getCell(row, column).setNeighbours(new Cell[]{
-//                    getCell(row - 1, numberOfColumns - 1), getCell(row - 1, column), getCell(row - 1, column + 1),
-//                    getCell(row, numberOfColumns - 1), getCell(row, column + 1),
-//                    getCell(0, numberOfColumns - 1), getCell(0, column), getCell(0, column + 1)
-//            });
-//        } else if (row == numberOfRows - 1 && column == numberOfColumns - 1) {     //down right corner
-//            getCell(row, column).setNeighbours(new Cell[]{
-//                    getCell(row - 1, numberOfColumns - 1), getCell(row - 1, column), getCell(row - 1, 0),
-//                    getCell(row, numberOfColumns - 1), getCell(row, 0),
-//                    getCell(0, numberOfColumns - 1), getCell(0, column), getCell(0, 0)
-//            });
-//        } else if (row == 0) {                                   // upper edge
-//            getCell(row, column).setNeighbours(new Cell[]{
-//                    getCell(numberOfRows - 1, column - 1), getCell(numberOfRows - 1, column), getCell(numberOfRows - 1, column + 1),
-//                    getCell(row, column - 1), getCell(row, column + 1),
-//                    getCell(row + 1, column - 1), getCell(row + 1, column), getCell(row + 1, column + 1)
-//            });
-//        } else if (row == numberOfRows - 1) {                    // down edge
-//            getCell(row, column).setNeighbours(new Cell[]{
-//                    getCell(row - 1, column - 1), getCell(row - 1, column), getCell(row - 1, column + 1),
-//                    getCell(row, column - 1), getCell(row, column + 1),
-//                    getCell(0, column - 1), getCell(0, column), getCell(0, column + 1)
-//            });
-//        } else if (column == 0) {                                // left edge
-//            getCell(row, column).setNeighbours(new Cell[]{
-//                    getCell(row - 1, numberOfColumns - 1), getCell(row - 1, column), getCell(row - 1, column + 1),
-//                    getCell(row, numberOfColumns - 1), getCell(row, column + 1),
-//                    getCell(row + 1, numberOfColumns - 1), getCell(row + 1, column), getCell(row + 1, column + 1)
-//            });
-//        } else if (column == numberOfColumns - 1) {                  // right edge
-//            getCell(row, column).setNeighbours(new Cell[]{
-//                    getCell(row - 1, column - 1), getCell(row - 1, column), getCell(row - 1, 0),
-//                    getCell(row, column - 1), getCell(row, 0),
-//                    getCell(row + 1, column - 1), getCell(row + 1, column), getCell(row + 1, 0)
-//            });
-//        } else {
-//            getCell(row, column).setNeighbours(new Cell[]{
-//                    getCell(row - 1, column - 1), getCell(row - 1, column), getCell(row - 1, column + 1),
-//                    getCell(row, column - 1), getCell(row, column + 1),
-//                    getCell(row + 1, column - 1), getCell(row + 1, column), getCell(row + 1, column + 1)
-//            });
-//        }
-//    }
 
 
     public Board playGame() {
